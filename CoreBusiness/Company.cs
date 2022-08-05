1b922ba07f562a778a9c3a99d5464749b0544913
prashanb <prashan.2017287@iit.ac.lk>
2022-08-05 01:33:09 -0700

    Add project files.

---
+++ CoreBusiness/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBusiness
+{
+    internal class Company
+    {
+        public int CompanyId { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+    }
+}
---
+++ UseCases/ViewCompaniesUseCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases
+{
+    internal class ViewCompaniesUseCase
+    {
+        public IEnumerable<Company> Execute()
+        {
+        }
+
+    }
+}
